be723063d3942de1c1085f7284d26cf20935f612
PoliceKelRP <policekelrp@gmail.com>
2024-12-01 11:58:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Mot de passe correct
-const correctPassword = "75N008";
+const correctPassword = "police08";
 
 // Récupération des éléments
 const privateLink = document.getElementById('privateLink');
